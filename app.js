f24500fc3232bbf4728d9b713f0fbc96aeb43f1a
Andrew TOwnsley <atownsley86@gmail.com>
2021-07-05 17:15:26 -0600

    working on sorting the country data by population

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const sortBtn = document.getElementById("sortBtn")
+const popSortBtn = document.getElementById("popSortBtn")
+const densityBtn = document.getElementById("densityBtn")
 const twentyBtn = document.getElementById("twentyBtn")
-const totalBtn = document.getElementById("totalBtn")
 const englishBtn = document.getElementById("englishBtn")
 const countryList = document.getElementById("countryList")
-
-
+let countryData = []
 fetchCountry()
 
 function fetchCountry() {
-    fetch("https://restcountries.eu/rest/v2/region/europe")
+    fetch("https://restcountries.eu/rest/v2/region/americas")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        console.log(data[0]);
+        console.log(data);
         displayCountry(data)
+        // countryData.push(data)
+        
+        data.forEach((item) => {
+            const newCountry = {
+                name: item.name,
+                population: item.population
+            }
+            countryData.push(newCountry);
+        })
+        console.log(countryData);
     })
 }
 
@@ -22,11 +31,23 @@ const displayCountry = (data) => {
     const countryList = document.getElementById("countryList");
     data.forEach((country) => {
             const countryItem = document.createElement('li')
+            countryItem.classList.add('country-item');
             countryItem.innerHTML =  
             `
-            <span>${country.name}</span><span>${country.population}</span>
+                <span>${country.name}</span>
+                <span> ${country.population}</span>
+                <span> ${country.area == null ? "N/A" :
+                (country.population / country.area).toFixed(2)}</span>
             `
+            //Area == null ? return N/A
             countryList.appendChild(countryItem);
         })
-
 }
+
+const populationSort = () => {
+    // console.log(countryData[0]);
+}
+
+popSortBtn.addEventListener("click", populationSort);
+
+
