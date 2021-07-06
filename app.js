34b32ab2a3f99bdbdc3f002655ee195311b4407a
Andrew TOwnsley <atownsley86@gmail.com>
2021-07-06 17:12:49 -0600

    Trying to figure out how to get the array of objects that is logged to the console to render on th UI.

--- app.js
+++ app.js
@@ -4,32 +4,67 @@ const twentyBtn = document.getElementById("twentyBtn")
 const englishBtn = document.getElementById("englishBtn")
 const countryList = document.getElementById("countryList")
 let countryData = []
+
 fetchCountry()
 
-function fetchCountry() {
-    fetch("https://restcountries.eu/rest/v2/region/americas")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        displayCountry(data)
-        // countryData.push(data)
+// function fetchCountry() {
+//     fetch("https://restcountries.eu/rest/v2/region/europe")
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         // console.log(data);
         
-        data.forEach((item) => {
-            const newCountry = {
-                name: item.name,
-                population: item.population
-            }
-            countryData.push(newCountry);
-        })
-        console.log(countryData);
+        // data.forEach((country) => {
+        //     const newCountry = {
+        //         name: country.name,
+        //         population: country.population
+        //     }
+        //     displayCountry(data)
+        //     pushData(newCountry)
+        // })
+//         console.log(countryData);
+//     })
+// }
+
+//============================================================================
+
+async function fetchCountry() {
+    const response = await fetch("https://restcountries.eu/rest/v2/region/europe");
+    const data = await response.json();
+
+    
+    data.forEach((country) => {
+        const newCountry = {
+            name: country.name,
+            population: country.population
+        }
+        displayCountry(data)
+        pushData(newCountry)
     })
 }
 
-const displayCountry = (data) => {
-    const countryList = document.getElementById("countryList");
-    data.forEach((country) => {
+
+//============================================================================
+
+const populationSort = () => {
+    let sortedPop = 
+    countryData.sort((a, b) => {
+        return (a.population > b.population) ? -1 : 1;
+    })
+    console.log(sortedPop);
+    // displayCountry() 
+}
+
+const pushData = (object) => {
+    countryData.push(object)
+    // displayCountry()
+}
+
+const displayCountry = (countryData) => {
+
+    countryList.innerHTML = "";
+    countryData.forEach((country) => {
             const countryItem = document.createElement('li')
             countryItem.classList.add('country-item');
             countryItem.innerHTML =  
@@ -39,13 +74,8 @@ const displayCountry = (data) => {
                 <span> ${country.area == null ? "N/A" :
                 (country.population / country.area).toFixed(2)}</span>
             `
-            //Area == null ? return N/A
             countryList.appendChild(countryItem);
         })
-}
-
-const populationSort = () => {
-    // console.log(countryData[0]);
 }
 
 popSortBtn.addEventListener("click", populationSort);
